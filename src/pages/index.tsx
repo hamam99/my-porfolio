78b26e5ccb82109938381fb1e41556ffeb3b3e41
Hamam <zero.saturnus@gmail.com>
2023-02-01 16:06:26 +0700

    add index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Navbar from '../components/Navbar'
+import Main from '../components/Main'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       </Head>
 
       <Navbar />
+      <Main />
     </>
   )
 }
