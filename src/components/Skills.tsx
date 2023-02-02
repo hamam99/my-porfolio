7d1a39b0a7a325cd983f21b519a6b9e024c8a901
Hamam <zero.saturnus@gmail.com>
2023-02-02 21:55:42 +0700

    refactor skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
+import SkillData from '../data/Skills'
+import SkillItems from './SkillItems'
 
 const Skills = () => {
   return (
@@ -10,85 +12,11 @@ const Skills = () => {
         </p>
         <h2 className="py-4">What I Can Do</h2>
         <div className="grid  md:grid-cols-2 lg:grid-cols-4 gap-8">
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap4- justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={'/../public/assets/skills/react.png'}
-                  alt="/"
-                  width={64}
-                  height={64}
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>React JS & React Native</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap4- justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={'/../public/assets/skills/firebase.png'}
-                  alt="/"
-                  width={64}
-                  height={64}
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Firebase</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap4- justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={'/../public/assets/skills/javascript.png'}
-                  alt="/"
-                  width={64}
-                  height={64}
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Javascript</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap4- justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={'/../public/assets/skills/nextjs.png'}
-                  alt="/"
-                  width={64}
-                  height={64}
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Next JS</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap4- justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={'/../public/assets/skills/tailwind.png'}
-                  alt="/"
-                  width={64}
-                  height={64}
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Tailwind CSS</h3>
-              </div>
-            </div>
-          </div>
+          {SkillData.map((data, index) => {
+            return (
+              <SkillItems skill={data.skill} image={data.image} key={index} />
+            )
+          })}
         </div>
       </div>
     </div>
---
+++ src/data/Skills.tsx
@@ -0,0 +1,40 @@
+import reactImage from '/public/assets/skills/react.png'
+import firebaseImage from '/public/assets/skills/firebase.png'
+import jsImage from '/public/assets/skills/javascript.png'
+import typescriptImage from '/public/assets/skills/typescript.png'
+import nextJSImage from '/public/assets/skills/nextjs.png'
+import tailWindCssImage from '/public/assets/skills/tailwind.png'
+import reduxImage from '/public/assets/skills/redux.png'
+
+const Skills = [
+  {
+    skill: 'React Native and React JS',
+    image: reactImage
+  },
+  {
+    skill: 'Firebase',
+    image: firebaseImage
+  },
+  {
+    skill: 'Javascript',
+    image: jsImage
+  },
+  {
+    skill: 'Typescript',
+    image: typescriptImage
+  },
+  {
+    skill: 'Redux',
+    image: reduxImage
+  },
+  {
+    skill: 'Next JS',
+    image: nextJSImage
+  },
+  {
+    skill: 'Tailwind CSS',
+    image: tailWindCssImage
+  }
+]
+
+export default Skills
