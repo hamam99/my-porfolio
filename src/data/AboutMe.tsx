3c652473f51be4562f8d1460d18bc8faecf95653
Hamam <zero.saturnus@gmail.com>
2023-02-02 21:55:49 +0700

    add mock data about me

--- src/data/AboutMe.tsx
+++ src/data/AboutMe.tsx
@@ -1,7 +1,8 @@
 const linkedin = 'https://www.linkedin.com/in/hamam-nasrodin-a9bb9811a/'
 const github = 'https://github.com/hamam99'
 const email = 'hamamnasrodins@gmail.com'
+const name = 'Hamam'
 
-const AboutMe = { linkedin, github, email }
+const AboutMe = { linkedin, github, email, name }
 
 export default AboutMe
